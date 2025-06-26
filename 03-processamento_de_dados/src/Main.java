77425f000239e3526e47c060667f1c04a00f93dc
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-06-26 18:30:36 -0300

    feat: Data processing and casting

---
+++ 03-processamento_de_dados/src/Main.java
@@ -0,0 +1,34 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*int x, y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);*/
+
+        /*double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println(area);*/
+
+        int a, b;
+        double result;
+
+        a = 5;
+        b = 2;
+
+        // Casting
+        result = (double) a / b;
+
+        System.out.println(result);
+    }
+}
